bd267963b4db081ab24ebf3b04f57bc693ba082a
Shannon Feng <redmeteor1206@gmail.com>
2023-07-23 01:38:28 -0700

    Add files via upload

---
+++ ex_05_02.py
@@ -0,0 +1,17 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        inum = int(num)
+    except:
+        print('Invalid input')
+        continue
+    if largest is None or inum > largest:
+        largest = inum
+    if smallest is None or inum < smallest:
+        smallest = inum
+print("Maximum is", largest)
+print("Minimum is", smallest)
